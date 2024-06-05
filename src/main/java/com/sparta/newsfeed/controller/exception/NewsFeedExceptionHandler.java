66bdf8a62839f4c6a5879818bf11bdfbfa2f3ba8
Han <nuher038@gmail.com>
2024-06-05 18:16:37 +0900

    스웨그 기능 및 예외처리 응답헨들러

---
+++ src/main/java/com/sparta/newsfeed/controller/exception/NewsFeedExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sparta.newsfeed.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
+
+@ControllerAdvice
+public class NewsFeedExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(Objects.requireNonNull(e.getFieldError()).getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
