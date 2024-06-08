90ae1e8a3de03bb4064c093a0480e73aaaae5ddc
Imnotcoderdude <topsolo7@naver.com>
2024-06-08 23:17:07 +0900

    여러가지 베이스 튜닝

--- src/main/java/com/sparta/newsfeed/dto/UserDto/SignUpRequestDto.java
+++ src/main/java/com/sparta/newsfeed/dto/UserDto/SignUpRequestDto.java
@@ -13,20 +13,21 @@ import lombok.Getter;
 public class SignUpRequestDto {
 
     //아이디
-    @NotBlank(message = "사용자 아이디는 비워둘 수 없습니다.")
-    @Size(min = 10, max = 20, message = "사용자 ID는 최소 10글자 이상, 최대 20글자 이하여야 합니다.")
+    @NotBlank(message = "userId는 비워둘 수 없습니다.")
+    @Size(min = 10, max = 20, message = "userId 는 최소 10글자 이상, 최대 20글자 이하여야 합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "사용자 ID는 대소문자 포함 영문과 숫자만 허용됩니다.")
     private String userId;
 
     // 비밀번호
-    @NotBlank(message = " 비밀번호는 비워둘 수 없습니다.")
-    @Size(min = 10, message = "비밀번호는 최소 10글자 이상이어야 합니다.")
+    @NotBlank(message = " password는 비워둘 수 없습니다.")
+    @Size(min = 10, message = "password는 최소 10글자 이상이어야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[~!@#$%^&*()_+])[A-Za-z\\d~!@#$%^&*()_+]{10,}$"
-            , message = "비밀번호는 대소문자 포함 영문, 숫자, 특수문자를 최소 1글자씩 포함해야 합니다.")
+            , message = "password는 대소문자 포함 영문, 숫자, 특수문자를 최소 1글자씩 포함해야 합니다.")
     private String password;
 
     //이름
     @NotBlank(message = " username 비워둘 수 없습니다.")
+    @Pattern(regexp = "^[\\w]+$", message = "username 공백을 포함할 수 없습니다.")
     private String username = "username";
 
     //이매일
