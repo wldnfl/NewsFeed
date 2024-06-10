8ddfb11f234be175d47d905c90cf0e391b6a5c6f
Han <nuher038@gmail.com>
2024-06-10 10:41:53 +0900

    예외처리 조건대로 변경

--- src/main/java/com/sparta/newsfeed/controller/exception/NewsFeedExceptionHandler.java
+++ src/main/java/com/sparta/newsfeed/controller/exception/NewsFeedExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sparta.newsfeed.controller.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,17 +13,26 @@ import java.util.Objects;
 public class NewsFeedExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(400, e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return new ResponseEntity<>(Objects.requireNonNull(e.getFieldError()).getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(400, e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorResponseDto> handleRuntimeException(RuntimeException e) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(500, e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponseDto> handleNullPointerException(NullPointerException e) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(500, e.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
