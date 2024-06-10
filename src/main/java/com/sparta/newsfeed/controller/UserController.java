67d92b81929fdb61b4dcf4dab22242a786ee2766
Imnotcoderdude <topsolo7@naver.com>
2024-06-10 11:50:46 +0900

    update : 회원가입 관련 API swagger summary 적용

--- src/main/java/com/sparta/newsfeed/controller/UserController.java
+++ src/main/java/com/sparta/newsfeed/controller/UserController.java
@@ -9,6 +9,7 @@ import com.sparta.newsfeed.dto.emaildto.EmailRequestDto;
 import com.sparta.newsfeed.dto.emaildto.ReVerifyEMailRequestDto;
 import com.sparta.newsfeed.service.SignUpService;
 import com.sparta.newsfeed.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -25,12 +26,14 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/user/sign")
+    @Operation(summary = "회원 가입")
     public String addUser(@Valid @RequestBody SignUpRequestDto requestDto) {
         return  signUpService.addUser(requestDto);
     }
 
     // 이메일 인증
     @PostMapping("/user/verify")
+    @Operation(summary = "이메일 인증")
     public String verifyEmail(@Valid @RequestBody EmailRequestDto requestDto) {
         signUpService.verifyEmail(requestDto);
         return "이메일 인증 성공";
@@ -38,24 +41,28 @@ public class UserController {
 
     // 이메일 인증 시간 초과시 인증번호 재요청 api
     @PostMapping("/user/reverify")
+    @Operation(summary = "이메일 인증 재요청")
     public String reverifyEmail(@Valid @RequestBody ReVerifyEMailRequestDto requestDto) {
         return signUpService.reverifyEmail(requestDto);
     }
 
     // 로그인
     @PostMapping("/user/login")
+    @Operation(summary = "로그인")
     public String loginUser(@Valid @RequestBody LoginUpRequestDto requestDto, HttpServletResponse response) {
         return signUpService.loginUser(requestDto, response);
     }
 
     // 로그아웃
     @PostMapping("/user/logout")
+    @Operation(summary = "로그아웃")
     public String logoutUser(/*@RequestHeader("AccessToken") String accessToken,*/ HttpServletRequest request,HttpServletResponse response) {
         return signUpService.logoutUser(request,response);
     }
 
     // 회원 탈퇴
     @PostMapping("/user/delete")
+    @Operation(summary = "회원 탈퇴")
     public String deleteUser(/*@RequestHeader("AccessToken") String accessToken,*/
             @RequestBody
             LoginUpRequestDto loginUpRequestDto,
@@ -66,12 +73,14 @@ public class UserController {
 
     // 유저 프로필 가져오기
     @GetMapping("/user/profile")
+    @Operation(summary = "유저 프로필 가져오기")
     public UserResponseDto getUserProfile(HttpServletRequest request) {
         return userService.getUserProfile(request);
     }
 
     // 유저 프로필 수정
     @PatchMapping("/user/profile")
+    @Operation(summary = "유저 프로필 수정")
     public String updateUserProfile(HttpServletRequest request, @RequestBody UserRequestDto userRequestDto){
         return userService.updateUserProfile(request,userRequestDto);
     }
